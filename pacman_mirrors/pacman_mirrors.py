298894eb7c0476c1b4712857f274c046dcc4b322
Ramon Buldó <rbuldo@gmail.com>
2015-08-08 08:01:32 +0200

    Add license headers

--- pacman_mirrors/pacman_mirrors.py
+++ pacman_mirrors/pacman_mirrors.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Release : 1.5.1
-# Date    : 28 March 2014
-# Authors : Esclapion, philm
+#
+# This file is part of pacman-mirrors.
+#
+# pacman-mirrors is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pacman-mirrors is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pacman-mirrors.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author(s): Esclapion
+#            philm
+#            Ramon Buldó <rbuldo@gmail.com
 
 import argparse
 import datetime
--- pacman_mirrors/pacman_mirrors_gui.py
+++ pacman_mirrors/pacman_mirrors_gui.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Release : 1.4
-# Date    : 10 Octobre 2013
-# Author  : Esclapion
+#
+# This file is part of pacman-mirrors.
+#
+# pacman-mirrors is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pacman-mirrors is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pacman-mirrors.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author(s): Esclapion
 
 from gi.repository import Gtk
 
