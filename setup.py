13da3559aeb07aec8fe620d7e501db630c2a3251
Ramon Buldó <rbuldo@gmail.com>
2015-08-16 20:41:33 +0200

    Install configuration file and mirrors with setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
+import glob
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -21,6 +18,8 @@ test_requirements = [
     # TODO: put package test requirements here
 ]
 
+mirror_files = glob.glob("data/mirrors/*")
+
 setup(
     name='pacman-mirrors',
     version='20150808',
@@ -29,27 +28,22 @@ setup(
     author="Roland Singer, Esclapion, philm, Ramon Buldó",
     author_email='ramon@manjaro.org',
     url='https://github.com/manjaro/pacman-mirrors',
-    packages=[
-        'pacman_mirrors',
-        'pacman_mirrors_gui'
-    ],
-    package_dir={'pacman_mirrors':
-                 'pacman_mirrors',
-                 'pacman_mirrors_gui':
-                 'pacman_mirrors'},
-    include_package_data=True,
-    data_files=[('/etc', ['conf/pacman-mirrors-conf'])],
+    packages=['pacman_mirrors'],
+    package_dir={'pacman_mirrors': 'pacman_mirrors'},
+    data_files=[('etc', ['conf/pacman-mirrors.conf']),
+                ('etc/pacman.d/mirrors', mirror_files)],
     install_requires=requirements,
     license="GPL3",
     zip_safe=False,
     keywords='pacman-mirrors',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End User/Desktop',
         'License :: OSI Approved :: GPL3 License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Environment :: Console'
     ],
     test_suite='tests',
     tests_require=test_requirements
