591c34425a849595002b655439cfebbdceb110d1
FH <frede@hundewadt.dk>
2017-02-12 15:27:28 +0100

    cleanup

--- pacman_mirrors/http_module.py
+++ pacman_mirrors/http_module.py
@@ -1,14 +1,12 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 """Manjaro-Mirrors HTTP Module"""
 
 import json
 import time
-from http.client import HTTPException
-from socket import timeout
-from urllib.error import URLError
-from urllib.request import urlopen
-# from collections import OrderedDict
-from .configuration import MIRRORS_URL, STATES_URL, MIRRORS_FILE, STATES_FILE
+import urllib.error
+import urllib.request
+import collections
+from .configuration import MIRRORS_URL, STATES_URL, MIRRORS_JSON, STATES_JSON
 from .local_module import FileHandler
 from . import txt
 
@@ -16,64 +14,67 @@ from . import txt
 class Fetcher():
     """Fetcher Class"""
 
-    def get_mirrors_list(self):
+    @staticmethod
+    def get_mirrors_list():
         """Retrieve mirror list from manjaro.org"""
         mirrors = list()
         try:
-            with urlopen(MIRRORS_URL) as response:
+            with urllib.request.urlopen(MIRRORS_URL) as response:
                 mirrors = json.loads(response.read().decode(
-                    "utf8")) #, object_pairs_hook=OrderedDict
+                    "utf8"), object_pairs_hook=collections.OrderedDict)
         except URLError:
             print("Error getting mirror list from server")
         if mirrors:
             print(mirrors)
-            FileHandler.write_json(mirrors, MIRRORS_FILE)
+            FileHandler.write_json(mirrors, MIRRORS_JSON)
 
-    def get_mirrors_state(self):
+    @staticmethod
+    def get_mirrors_state():
         """Retrieve state for all mirrors from manjaro.org"""
         states = list()
         try:
-            with urlopen(STATES_URL) as response:
+            with urllib.request.urlopen(STATES_URL) as response:
                 states = json.loads(
                     response.read().decode(
-                        "utf8")) # , object_pairs_hook=OrderedDict
+                        "utf8"), object_pairs_hook=collections.OrderedDict)
         except URLError:
             print("Error getting mirrors state from server")
         if states:
-            FileHandler.write_json(states, STATES_FILE)
+            FileHandler.write_json(states, STATES_JSON)
 
-    def get_response_time(self, _mirror_url, _timeout, _quiet):
+    @staticmethod
+    def get_response_time(mirror_url, timeout, quiet):
         """Get a mirrors response time
 
-        :param _mirror_url: mirrors url
-        :param _timeout: wait for mirror response
-        :param _quiet: controls message output
+        :param mirror_url: mirrors url
+        :param timeout: wait for mirror response
+        :param quiet: controls message output
         :return string: response time
         """
         probe_start = time.time()
         probe_time = txt.SERVER_RES
         try:
-            urlopen(_mirror_url, timeout=_timeout)
+            urllib.request.urlopen(mirror_url, timeout=timeout)
             probe_stop = time.time()
-        except URLError as err:
+        except urllib.request.URLError as err:
             probe_stop = None
-            if hasattr(err, "reason") and not _quiet:
+            if hasattr(err, "reason") and not quiet:
                 print("\n{}: {}: {}".format(txt.ERROR,
                                             txt.ERR_SERVER_NOT_REACHABLE,
                                             err.reason))
-            elif hasattr(err, "code") and not _quiet:
+            elif hasattr(err, "code") and not quiet:
                 print("\n{}: {}: {}".format(txt.ERROR,
                                             txt.ERR_SERVER_REQUEST,
                                             err.errno))
         except timeout:
             probe_stop = None
-            if not _quiet:
+            if not quiet:
                 print("\n{}: {}: {}".format(txt.ERROR,
                                             txt.ERR_SERVER_NOT_AVAILABLE,
                                             txt.TIMEOUT))
         except HTTPException:
             probe_stop = None
-            if not _quiet:
+            if not quiet:
                 print("\n{}: {}: {}".format(txt.ERROR,
                                             txt.ERR_SERVER_HTTP_EXCEPTION,
                                             txt.HTTP_EXCEPTION))
