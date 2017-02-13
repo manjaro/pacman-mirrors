973a075a26d5615bc869ed55573a7760304e1d48
FH <frede@hundewadt.dk>
2017-02-13 12:41:19 +0100

    MirrorList Class

---
+++ pacman_mirrors/mirror_list.py
@@ -0,0 +1,35 @@
+#!/usr/env python3
+"""Pacman-Mirrors MirrorList Module"""
+
+
+class MirrorList:
+    """Pacman-Mirrors MirrorLists Class"""
+
+    def __init__(self):
+        self.mirrorlist = []
+
+    def cls_add_country(self, country):
+        """Append country"""
+        self.mirrorlist.append({
+            "country": country,
+            "mirrors": []
+        })
+
+    def cls_add_country_mirror(self, country, url, protocols, last_sync="99:99",
+                               stable="0", testing="0", unstable="0"):
+        """Append mirror"""
+        mirror = {
+            "mirror": url,
+            "protocols": protocols,
+            "last_sync": last_sync,
+            "branches": {
+                "stable": stable,
+                "testing": testing,
+                "unstable": unstable
+            },
+        }
+        self.mirrorlist[country]["mirrors"].append(mirror)
+
+    def get_mirrorlist(self):
+        """Return mirrorlist"""
+        return self.mirrorlist
