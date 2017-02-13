877ba15c2683cbdcc0103c1db57919e6043ac4dc
FH <frede@hundewadt.dk>
2017-02-13 21:19:09 +0100

    how does manjaro-mirrors sound added fallback

--- pacman_mirrors/configuration.py
+++ pacman_mirrors/configuration.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python3
-"""Pacman-Mirrors Configuration"""
+"""Manjaro-Mirrors Configuration"""
 ENV = "dev"
 # http constants
 URL_MIRROR_JSON = "http://repo.manjaro.org/mirrors.json"
 URL_STATUS_JSON = "http://repo.manjaro.org/status.json"
-# local file constants
+
 if ENV == "production":
+    # dir constant
     MIRROR_DIR = "/var/lib/manjaro-mirrors/"
-    O_CUST_FILE = "/var/lib/pacman-mirrors/Custom"
+    # file constant
     CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
-    CONFIG_FILE = "/etc/pacman-mirrors"
-    MIRROR_FILE = MIRROR_DIR + "mirrors.json"
+    CONFIG_FILE = "/etc/pacman-mirrors.conf"
+    HTTP_FALLBACK = MIRROR_DIR + "mirrors.json"
+    MIRROR_FILE = "/usr/lib/manjaro-mirrors/mirrors.json"
     MIRROR_LIST = "/etc/pacman.d/mirrorlist"
     STATUS_FILE = MIRROR_DIR + "status.json"
+    # special case
+    O_CUST_FILE = "/var/lib/pacman-mirrors/Custom"
 else:
-    MIRROR_DIR = "data/manjaro-mirrors/"
-    O_CUST_FILE = "data/pacman-mirrors/Custom"
+    # dir constant
+    MIRROR_DIR = "dev/var/lib/manjaro-mirrors/"
+    # file constant
     CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
-    CONFIG_FILE = "conf/pacman-mirrors.conf"
-    MIRROR_FILE = MIRROR_DIR + "mirrors.json"
-    MIRROR_LIST = "data/mirrorlist"
+    CONFIG_FILE = "dev/etc/pacman-mirrors.conf"
+    HTTP_FALLBACK = MIRROR_DIR + "mirrors.json"
+    MIRROR_FILE = "dev/usr/lib/manjaro-mirrors/mirrors.json"
+    MIRROR_LIST = "dev/etc/pacman.d/mirrorlist"
     STATUS_FILE = MIRROR_DIR + "status.json"
+    # special case
+    O_CUST_FILE = "dev/var/lib/pacman-mirrors/Custom"
 # repo constants
 BRANCHES = ("stable", "testing", "unstable")
 REPO_ARCH = "$repo/$arch"
