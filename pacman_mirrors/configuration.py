a6a38ee3dc0b6e496ef1413e9d984bb1935f375d
Frede H <fhdk@users.noreply.github.com>
2017-02-13 22:10:51 +0100

    Update configuration.py

--- pacman_mirrors/configuration.py
+++ pacman_mirrors/configuration.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Manjaro-Mirrors Configuration"""
+"""Pacman-Mirrors Configuration"""
 ENV = "dev"
 # http constants
 URL_MIRROR_JSON = "http://repo.manjaro.org/mirrors.json"
@@ -7,24 +7,24 @@ URL_STATUS_JSON = "http://repo.manjaro.org/status.json"
 
 if ENV == "production":
     # dir constant
-    MIRROR_DIR = "/var/lib/manjaro-mirrors/"
+    MIRROR_DIR = "/var/lib/pacman-mirrors/"
     # file constant
     CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
     CONFIG_FILE = "/etc/pacman-mirrors.conf"
     HTTP_FALLBACK = MIRROR_DIR + "mirrors.json"
-    MIRROR_FILE = "/usr/lib/manjaro-mirrors/mirrors.json"
+    MIRROR_FILE = "/usr/lib/pacman-mirrors/mirrors.json"
     MIRROR_LIST = "/etc/pacman.d/mirrorlist"
     STATUS_FILE = MIRROR_DIR + "status.json"
     # special case
     O_CUST_FILE = "/var/lib/pacman-mirrors/Custom"
 else:
     # dir constant
-    MIRROR_DIR = "dev/var/lib/manjaro-mirrors/"
+    MIRROR_DIR = "dev/var/lib/pacman-mmirrors/"
     # file constant
     CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
     CONFIG_FILE = "dev/etc/pacman-mirrors.conf"
     HTTP_FALLBACK = MIRROR_DIR + "mirrors.json"
-    MIRROR_FILE = "dev/usr/lib/manjaro-mirrors/mirrors.json"
+    MIRROR_FILE = "dev/usr/lib/pacman-mirrors/mirrors.json"
     MIRROR_LIST = "dev/etc/pacman.d/mirrorlist"
     STATUS_FILE = MIRROR_DIR + "status.json"
     # special case
