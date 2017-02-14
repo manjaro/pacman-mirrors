dce216ac8ba268e0d0d8549877afab71b4d51850
FH <frede@hundewadt.dk>
2017-02-14 10:16:12 +0100

    skipped the separate country thing

--- pacman_mirrors/customfile.py
+++ pacman_mirrors/customfile.py
@@ -3,7 +3,7 @@
 
 import os
 from .configuration import ENV, O_CUST_FILE, CUSTOM_FILE, MIRROR_DIR
-from .mirrorlist import MirrorList
+from .mirrors import Mirrors
 from .filemethods import FileMethods
 
 
@@ -13,23 +13,24 @@ class CustomFile:
         """Convert custom mirror file to json"""
         if os.path.isfile(O_CUST_FILE):
             with open(O_CUST_FILE, "r") as mirrorfile:
-                mirror = MirrorList()
+                mirror = Mirrors()
                 mirror_country = None
                 for line in mirrorfile:
                     country = ImportHelper.get_country(line)
                     if country:
-                        if mirror_country != country:  # add only if different
-                            mirror.add_country(country)
                         mirror_country = country
                         continue
                     mirror_url = ImportHelper.get_url(line)
                     if not mirror_url:
                         continue
                     mirror_protocol = ImportHelper.get_protocol(mirror_url)
-                    mirror.add_mirror(country, mirror_url, [mirror_protocol])
-                FileMethods.write_json(mirror.get_mirrorlist(), CUSTOM_FILE)
-                if ENV == "production":
-                    ImportHelper.cleanup()
+
+                    mirror.add_mirror(mirror_country, mirror_url, [mirror_protocol])
+
+                FileMethods.write_json(mirror.get_mirrors(), CUSTOM_FILE)
+                print("TODO: change this")
+                print("calling ImportHelper.cleanup()")
+                # ImportHelper.cleanup()
 
 
 class ImportHelper:
