a1bf0841397ffcd43aacd1059b5ca7c1e068a7f3
FH <frede@hundewadt.dk>
2017-02-14 09:20:13 +0100

    realigned files

--- pacman_mirrors/configuration.py
+++ pacman_mirrors/configuration.py
@@ -6,29 +6,29 @@ URL_MIRROR_JSON = "http://repo.manjaro.org/mirrors.json"
 URL_STATUS_JSON = "http://repo.manjaro.org/status.json"
 
 if ENV == "production":
-    # dir constant
-    MIRROR_DIR = "/var/lib/pacman-mirrors/"
-    # file constant
-    CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
+    # etc
     CONFIG_FILE = "/etc/pacman-mirrors.conf"
-    HTTP_FALLBACK = MIRROR_DIR + "mirrors.json"
-    MIRROR_FILE = "/usr/lib/pacman-mirrors/mirrors.json"
     MIRROR_LIST = "/etc/pacman.d/mirrorlist"
-    STATUS_FILE = MIRROR_DIR + "status.json"
-    # special case
-    O_CUST_FILE = "/var/lib/pacman-mirrors/Custom"
-else:
-    # dir constant
-    MIRROR_DIR = "/pacman-mirrors/"
-    # file constant
+    # pacman-mirrors
+    MIRROR_DIR = "/var/lib/pacman-mirrors/"
     CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
-    CONFIG_FILE = "root/etc/pacman-mirrors.conf"
-    HTTP_FALLBACK = MIRROR_DIR + "mirrors.json"
-    MIRROR_FILE = "root/usr/lib/pacman-mirrors/mirrors.json"
-    MIRROR_LIST = "root/etc/pacman.d/mirrorlist"
+    MIRROR_FILE = MIRROR_DIR + "mirrors.json"
     STATUS_FILE = MIRROR_DIR + "status.json"
-    # special case
-    O_CUST_FILE = "root/var/lib/pacman-mirrors/Custom"
+    # special cases
+    FALLBACK = "/usr/share/pacman-mirrors/mirrors.json"
+    O_CUST_FILE = MIRROR_DIR + "Custom"
+else:
+    # etc
+    CONFIG_FILE = "mock/etc/pacman-mirrors.conf"
+    MIRROR_LIST = "mock/etc/mirrorlist"
+    # pacman-mirrors
+    MIRROR_DIR = "mock/var/"
+    CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
+    MIRROR_FILE = MIRROR_DIR + "mirrors.json"
+    STATUS_FILE = MIRROR_DIR + "status.json"
+    # special cases
+    FALLBACK = "mock/usr/mirrors.json"
+    O_CUST_FILE = MIRROR_DIR + "Custom"
 # repo constants
 BRANCHES = ("stable", "testing", "unstable")
 REPO_ARCH = "$repo/$arch"
