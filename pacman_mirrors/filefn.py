3cddeef4b5193c4bd051df747b00a049c07f4b91
FH <frede@hundewadt.dk>
2017-02-15 07:46:05 +0100

    Moved functions back in pacman_mirrors.py

--- pacman_mirrors/filefn.py
+++ pacman_mirrors/filefn.py
@@ -2,7 +2,6 @@
 
 """Manjaro-Mirrors Local Module"""
 
-import datetime
 import os
 import tempfile
 from . import txt
@@ -54,45 +53,3 @@ class FileFn:
             print("{}: {}: {}: {}".format(txt.ERROR, txt.ERR_FILE_READ,
                                           err.filename, err.strerror))
             exit(1)
-
-    @staticmethod
-    def write_mirror_list_header(handle, custom=False):
-        """
-        Write mirrorlist header
-
-        :param handle: handle to a file opened for writing
-        :param custom: controls content of the header
-        """
-        handle.write("##\n")
-        if custom:
-            handle.write("## Manjaro Linux Custom mirrorlist\n")
-            handle.write("## Generated on {}\n".format(
-                datetime.datetime.now().strftime("%d %B %Y %H:%M")))
-            handle.write("##\n")
-            handle.write("## Use 'pacman-mirrors -c all' to reset\n")
-        else:
-            handle.write("## Manjaro Linux mirrorlist\n")
-            handle.write("## Generated on {}\n".format(
-                datetime.datetime.now().strftime("%d %B %Y %H:%M")))
-            handle.write("##\n")
-            handle.write("## Use pacman-mirrors to modify\n")
-        handle.write("##\n\n")
-
-    @staticmethod
-    def write_mirror_list_server(handle, mirror):
-        """
-        Write mirror to mirror list or file
-
-        :param handle: handle to a file opened for writing
-        :param mirror: mirror object
-        """
-        work = mirror
-        handle.write("## Country       : {}\n".format(work["country"]))
-        if work["response_time"] == txt.SERVER_RES:
-            work["response_time"] = "N/A"
-        handle.write("## Response time : {}\n".format(work["response_time"]))
-        if work["last_sync"] == txt.SERVER_BAD or \
-                work["last_sync"] == txt.LASTSYNC_NA:
-            work["last_sync"] = "N/A"
-        handle.write("## Last sync     : {}h\n".format(work["last_sync"]))
-        handle.write("Server = {}\n\n".format(work["url"]))
