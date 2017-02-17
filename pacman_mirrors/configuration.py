6903761b5ecc8e6adc0c185ff35e90607d9cc6e7
FH <frede@hundewadt.dk>
2017-02-17 16:12:43 +0100

    this is needed for tests to work - for now anyway

--- pacman_mirrors/configuration.py
+++ pacman_mirrors/configuration.py
@@ -19,20 +19,40 @@
 
 """Pacman-Mirrors Configuration"""
 
-# http constants
-URL_MIRROR_JSON = "http://repo.manjaro.org/mirrors.json"
-URL_STATUS_JSON = "http://repo.manjaro.org/status.json"
-# etc
-CONFIG_FILE = "/etc/pacman-mirrors.conf"
-MIRROR_LIST = "/etc/pacman.d/mirrorlist"
-# pacman-mirrors
-MIRROR_DIR = "/var/lib/pacman-mirrors/"
-CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
-MIRROR_FILE = MIRROR_DIR + "mirrors.json"
-STATUS_FILE = MIRROR_DIR + "status.json"
-# special cases
-FALLBACK = "/usr/share/pacman-mirrors/mirrors.json"
-O_CUST_FILE = MIRROR_DIR + "Custom"
-# repo constants
-BRANCHES = ("stable", "testing", "unstable")
-REPO_ARCH = "/$repo/$arch"
+ENV = "dev"
+if ENV:
+    # http constants
+    URL_MIRROR_JSON = "http://repo.manjaro.org/mirrors.json"
+    URL_STATUS_JSON = "http://repo.manjaro.org/status.json"
+    # etc
+    CONFIG_FILE = "mock/etc/pacman-mirrors.conf"
+    MIRROR_LIST = "mock/etc/mirrorlist"
+    # pacman-mirrors
+    MIRROR_DIR = "mock/var/pacman-mirrors/"
+    CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
+    MIRROR_FILE = MIRROR_DIR + "mirrors.json"
+    STATUS_FILE = MIRROR_DIR + "status.json"
+    # special cases
+    FALLBACK = "mock/usr/mirrors.json"
+    O_CUST_FILE = MIRROR_DIR + "Custom"
+    # repo constants
+    BRANCHES = ("stable", "testing", "unstable")
+    REPO_ARCH = "/$repo/$arch"
+else:
+    # http constants
+    URL_MIRROR_JSON = "http://repo.manjaro.org/mirrors.json"
+    URL_STATUS_JSON = "http://repo.manjaro.org/status.json"
+    # etc
+    CONFIG_FILE = "/etc/pacman-mirrors.conf"
+    MIRROR_LIST = "/etc/pacman.d/mirrorlist"
+    # pacman-mirrors
+    MIRROR_DIR = "/var/lib/pacman-mirrors/"
+    CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
+    MIRROR_FILE = MIRROR_DIR + "mirrors.json"
+    STATUS_FILE = MIRROR_DIR + "status.json"
+    # special cases
+    FALLBACK = "/usr/share/pacman-mirrors/mirrors.json"
+    O_CUST_FILE = MIRROR_DIR + "Custom"
+    # repo constants
+    BRANCHES = ("stable", "testing", "unstable")
+    REPO_ARCH = "/$repo/$arch"
