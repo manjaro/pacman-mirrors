a25923258965eda2efd4751f51a2b0f4e66634ab
FH <frede@hundewadt.dk>
2017-02-17 14:12:53 +0100

    updated tests

--- tests/test_pacman_mirrors.py
+++ tests/test_pacman_mirrors.py
@@ -24,7 +24,7 @@ class TestPacmanMirrors(unittest.TestCase):
         mock_os_getuid.return_value = 0
         with unittest.mock.patch("sys.argv",
                                  ["pacman-mirrors",
-                                  "-m", "random"]):
+                                  "-m", "random", "--quiet"]):
             app = pacman_mirrors.PacmanMirrors()
             # if os.path.isfile(O_CUST_FILE):
             #     app.CustomFn.convert_to_json()
@@ -42,6 +42,21 @@ class TestPacmanMirrors(unittest.TestCase):
             else:
                 app.gen_mirror_list_common()
 
+    @patch("os.getuid")
+    def test_run_country(self, mock_os_getuid):
+        """Single country via commandline"""
+        mock_os_getuid.return_value = 0
+        with unittest.mock.patch("sys.argv",
+                                 ["pacman-mirrors",
+                                  "-c", "Germany"]):
+            app = pacman_mirrors.PacmanMirrors()
+            app.config = app.load_conf()
+            app.command_line_parse()
+            app.load_mirror_file()
+            app.validate_custom_config()
+            app.validate_country_selection()
+            assert app.only_country == ["Germany"]
+
     @patch.object(pacman_mirrors.HttpFn, "get_geoip_country")
     def test_get_geoip_country(self, mock_geoip):
         """Geoip country IS available"""
@@ -55,8 +70,22 @@ class TestPacmanMirrors(unittest.TestCase):
             app.load_mirror_file()
             app.validate_custom_config()
             app.validate_country_selection()
-            app.gen_server_lists()
             assert app.only_country == ["France"]
+
+    @patch.object(pacman_mirrors.HttpFn, "get_geoip_country")
+    def test_get_geoip_country(self, mock_geoip):
+        """Geoip country IS NOT available"""
+        mock_geoip.return_value = "Antartica"
+        with unittest.mock.patch("sys.argv",
+                                 ["pacman-mirrors",
+                                  "--geoip"]):
+            app = pacman_mirrors.PacmanMirrors()
+            app.config = app.load_conf()
+            app.command_line_parse()
+            app.load_mirror_file()
+            app.validate_custom_config()
+            app.validate_country_selection()
+            assert app.only_country == app.mirrors.countrylist
 
     def tearDown(self):
         pass
