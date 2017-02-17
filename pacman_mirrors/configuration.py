16551ca18e33190a5a3cd01795f76e96cc682fc0
FH <frede@hundewadt.dk>
2017-02-17 14:28:35 +0100

    changed configuration for release

--- pacman_mirrors/configuration.py
+++ pacman_mirrors/configuration.py
@@ -1,40 +1,19 @@
 #!/usr/bin/env python3
 """Pacman-Mirrors Configuration Module"""
-import os
-import datetime
-import tempfile
-from . import txt
-
-ENV = "dev"
 # http constants
 URL_MIRROR_JSON = "http://repo.manjaro.org/mirrors.json"
 URL_STATUS_JSON = "http://repo.manjaro.org/status.json"
-
-if ENV == "dev":
-    # etc
-    CONFIG_FILE = "mock/etc/pacman-mirrors.conf"
-    MIRROR_LIST = "mock/etc/mirrorlist"
-    # pacman-mirrors
-    MIRROR_DIR = "mock/var/"
-    CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
-    MIRROR_FILE = MIRROR_DIR + "mirrors.json"
-    STATUS_FILE = MIRROR_DIR + "status.json"
-    # special cases
-    FALLBACK = "mock/usr/mirrors.json"
-    O_CUST_FILE = MIRROR_DIR + "Custom"
-else:
-    # etc
-    CONFIG_FILE = "/etc/pacman-mirrors.conf"
-    MIRROR_LIST = "/etc/pacman.d/mirrorlist"
-    # pacman-mirrors
-    MIRROR_DIR = "/var/lib/pacman-mirrors/"
-    CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
-    MIRROR_FILE = MIRROR_DIR + "mirrors.json"
-    STATUS_FILE = MIRROR_DIR + "status.json"
-    # special cases
-    FALLBACK = "/usr/share/pacman-mirrors/mirrors.json"
-    O_CUST_FILE = MIRROR_DIR + "Custom"
-
+# etc
+CONFIG_FILE = "/etc/pacman-mirrors.conf"
+MIRROR_LIST = "/etc/pacman.d/mirrorlist"
+# pacman-mirrors
+MIRROR_DIR = "/var/lib/pacman-mirrors/"
+CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
+MIRROR_FILE = MIRROR_DIR + "mirrors.json"
+STATUS_FILE = MIRROR_DIR + "status.json"
+# special cases
+FALLBACK = "/usr/share/pacman-mirrors/mirrors.json"
+O_CUST_FILE = MIRROR_DIR + "Custom"
 # repo constants
 BRANCHES = ("stable", "testing", "unstable")
 REPO_ARCH = "/$repo/$arch"
