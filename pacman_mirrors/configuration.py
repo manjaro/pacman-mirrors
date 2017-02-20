b26cdc2148f3573b2150ddffd4966b37c23438c6
FH <frede@hundewadt.dk>
2017-02-20 20:16:47 +0100

    refactor objective testability Since travis builds are prone to fail if pacman-mirrors.conf is not found  It has been made possible to change location by adding it to internal config  It can then be changed on-the-fly to fit travis build (reverted from commit 9e9f32d55caf3d47080fba329b418bc2290a0457) (reverted from commit 625f1b3ecfd354fe20c8cecac5d23509a7a1fc1d)

--- pacman_mirrors/configuration.py
+++ pacman_mirrors/configuration.py
@@ -28,16 +28,16 @@ if DEVELOPMENT:
     URL_MIRROR_JSON = "http://repo.manjaro.org/mirrors.json"
     URL_STATUS_JSON = "http://repo.manjaro.org/status.json"
     # etc
-    CONFIG_FILE = "mock/pacman-mirrors.conf"
-    MIRROR_LIST = "mock/mirrorlist"
+    CONFIG_FILE = "mock/etc/pacman-mirrors.conf"
+    MIRROR_LIST = "mock/etc/mirrorlist"
     # pacman-mirrors
-    MIRROR_DIR = "mock/"
+    MIRROR_DIR = "mock/var/"
     CUSTOM_FILE = MIRROR_DIR + "custom-mirrors.json"
     MIRROR_FILE = MIRROR_DIR + "mirrors.json"
     STATUS_FILE = MIRROR_DIR + "status.json"
     # special cases
     O_CUST_FILE = MIRROR_DIR + "Custom"
-    FALLBACK = "mock/mirrors.json"
+    FALLBACK = "mock/usr/mirrors.json"
     # repo constants
     BRANCHES = ("stable", "testing", "unstable")
     REPO_ARCH = "/$repo/$arch"
