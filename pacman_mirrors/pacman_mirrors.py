234983d90d91cd3ab42ad50c0a447a01171e45e9
fhdk <fh@uex.dk>
2017-02-27 08:25:24 +0100

    centralized network check and appropiate message

--- pacman_mirrors/pacman_mirrors.py
+++ pacman_mirrors/pacman_mirrors.py
@@ -186,15 +186,9 @@ class PacmanMirrors:
         worklist = mirrorfn.filter_mirror_list(self.mirrors.mirrorlist,
                                                self.selected_countries)
         if self.config["method"] == "rank":
-            if self.network:
-                worklist = self.test_mirrors(worklist)
-                worklist = sorted(worklist, key=itemgetter("resp_time"))
-            else:
-                miscfn.internet_message()
-                miscfn.shuffle_message()
-                shuffle(worklist)
+            worklist = self.test_mirrors(worklist)
+            worklist = sorted(worklist, key=itemgetter("resp_time"))
         else:
-            miscfn.shuffle_message()
             shuffle(worklist)
 
         filefn.output_mirror_list(self.config["branch"],
@@ -210,37 +204,34 @@ class PacmanMirrors:
 
     def build_fasttrack_mirror_list(self, number):
         """Fast-track the mirrorlist by aggressive sorting"""
-        if self.network:
-            temp = sorted(self.mirrors.mirrorlist,
-                          key=itemgetter("branches", "last_sync"), reverse=True)
-            temp = sorted(temp, key=itemgetter("last_sync"), reverse=False)
-            ftlist = []
-            print(".: {}: {} - {}".format(txt.INF_CLR,
-                                          txt.QUERY_MIRRORS,
-                                          txt.TAKES_TIME))
-            counter = 0
-            for mirror in temp:
-                resp_time = httpfn.get_mirror_response(mirror["url"],
-                                                       quiet=self.quiet)
-                print("   ..... {:<15}: {}: {}".format(mirror["country"],
-                                                       mirror["last_sync"],
-                                                       mirror["url"]), end='')
-                sys.stdout.flush()
-                mirror["resp_time"] = resp_time
-                print("\r   {:<5}{}{} ".format(txt.GS, resp_time, txt.CE))
-                if resp_time == txt.SERVER_RES:
-                    continue
-                ftlist.append(mirror)
-                counter += 1
-                if counter == number:
-                    break
-            ftlist = sorted(ftlist, key=itemgetter("resp_time"))
-            filefn.output_mirror_list(self.config["branch"],
-                                      self.config["mirror_list"],
-                                      ftlist,
-                                      self.quiet)
-        else:
-            miscfn.internet_message(True)
+        temp = sorted(self.mirrors.mirrorlist,
+                      key=itemgetter("branches", "last_sync"), reverse=True)
+        temp = sorted(temp, key=itemgetter("last_sync"), reverse=False)
+        ftlist = []
+        print(".: {}: {} - {}".format(txt.INF_CLR,
+                                      txt.QUERY_MIRRORS,
+                                      txt.TAKES_TIME))
+        counter = 0
+        for mirror in temp:
+            resp_time = httpfn.get_mirror_response(mirror["url"],
+                                                   quiet=self.quiet)
+            print("   ..... {:<15}: {}: {}".format(mirror["country"],
+                                                   mirror["last_sync"],
+                                                   mirror["url"]), end='')
+            sys.stdout.flush()
+            mirror["resp_time"] = resp_time
+            print("\r   {:<5}{}{} ".format(txt.GS, resp_time, txt.CE))
+            if resp_time == txt.SERVER_RES:
+                continue
+            ftlist.append(mirror)
+            counter += 1
+            if counter == number:
+                break
+        ftlist = sorted(ftlist, key=itemgetter("resp_time"))
+        filefn.output_mirror_list(self.config["branch"],
+                                  self.config["mirror_list"],
+                                  ftlist,
+                                  self.quiet)
 
     def build_interactive_mirror_list(self):
         """Prompt the user to select the mirrors with a gui.
@@ -251,13 +242,8 @@ class PacmanMirrors:
         worklist = mirrorfn.filter_mirror_list(self.mirrors.mirrorlist,
                                                self.selected_countries)
         if self.config["method"] == "rank":
-            if self.network:
-                worklist = self.test_mirrors(worklist)
-                worklist = sorted(worklist, key=itemgetter("resp_time"))
-            else:
-                miscfn.internet_message()
-                miscfn.shuffle_message()
-                shuffle(worklist)
+            worklist = self.test_mirrors(worklist)
+            worklist = sorted(worklist, key=itemgetter("resp_time"))
         else:
             miscfn.shuffle_message()
             shuffle(worklist)
@@ -393,19 +379,28 @@ class PacmanMirrors:
         filefn.dir_must_exist(conf.MIRROR_DIR)
         self.config = configfn.build_config()
         self.command_line_parse()
-        self.network = httpfn.ping_host("repo.manjaro.org", 3)
         self.load_all_mirrors()
+        # network check
+        self.network = httpfn.ping_host("google.com", 3)
+        # all methods is available
         if self.network:
             httpfn.update_mirrors()
-
-        # actual generation
-        if self.fasttrack:
-            self.build_fasttrack_mirror_list(self.fasttrack)
-        else:
+            # actual generation
+            if self.fasttrack:
+                self.build_fasttrack_mirror_list(self.fasttrack)
+            else:
+                if self.interactive:
+                    self.build_interactive_mirror_list()
+                else:
+                    self.build_common_mirror_list()
+        # only random is available if network is down
+        if not self.network and self.config["method"] == "random":
             if self.interactive:
                 self.build_interactive_mirror_list()
             else:
                 self.build_common_mirror_list()
+        else:  # print message
+            miscfn.internet_message()
 
         print("{}.:! Pacman-Mirrors {} - {} {}".format(txt.YS,
                                                        __version__,
